62a98479647fec31266f3e9b947961b2ebde1fe6
Tisima <zerosan487@gmail.com>
2022-07-22 08:27:23 +0000

    Add:items/showにコードを追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,12 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+     # ページネーションをつけたいデータに.page(params[:page])を追加.https://qiita.com/rio_threehouse/items/313824b90a31268b0074
+    @items = Item.all.page(params[:page]).per(8)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 end
