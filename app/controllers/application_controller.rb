2335224de16e9e7dcc02cacd3c5c2fe9a072a573
EC2 Default User <ec2-user@ip-172-31-44-188.ap-northeast-1.compute.internal>
2022-07-23 10:58:04 +0000

    7/23Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+      if current_customer
+        flash[:notice] = "ログインに成功しました" 
+        root_path
+      else
+        flash[:notice] = "新規登録完了しました。次は名前の入力が必要です" 
+        root_path
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,30 @@
 Rails.application.routes.draw do
-  
 # 顧客用
-# URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 # 管理者用
-# URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+  # scope module: 'customers' do
+  #   root 'items#top'
+  #   resources :items, only: [:show, :index]
+  #   get 'about' => 'items#about'
+  # end
+
+
 #cart_items
 scope module: :public do
+  root to: 'homes#top'
   resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
 end
+
+#admin
+namespace :admin do
+  root :to =>"homes#top"
+end
+
 end
