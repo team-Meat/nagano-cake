0465da201259968e18dcf0f8d8f9e9334a524a4d
EC2 Default User <krat1021007@gmail.com>
2022-07-23 05:47:10 +0000

    ボタン編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,27 +1,27 @@
 class Public::CustomersController < ApplicationController
-  # before_action :correct_user, only: [:edit,:update]
+   before_action :current_customer, only: [:edit,:update]
   def show
-    #@customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-     #@customer = Customer.find(params[:id])
-     #@customer == current_user
+     @customer = Customer.find(params[:id])
+     @customer == current_customer
   end
 
   def update
-   #@customer = Customer(params[:id])
-  #   if@customer.update(customer_params)
-  #   redirect_to user_path(@user.id),notice:"You have updated user successfully."
-  #   else
-  #   render:edit
-  #     @customer == current_customer
-  #   end
+   @customer = Customer(params[:id])
+     if@customer.update(customer_params)
+     redirect_to customer_path(@customer.id),notice:"You have updated user successfully."
+     else
+     render:edit
+     @customer == current_customer
+     end
   end
 
-  # private
-  # def user_params
-  #   params.require(:user).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:postcode,:address,:phone_number,:is_deleted)
-  # end
+   private
+   def customer_params
+     params.require(:user).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:postcode,:address,:phone_number,:is_deleted)
+   end
 
 end
