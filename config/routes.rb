27fb527187bae844e4a5eb0b5062f3428a63efd5
Tisima <zerosan487@gmail.com>
2022-07-23 05:51:14 +0000

    Additems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :admins
   # devise_for :customers
-  
+
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -14,7 +14,8 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+
   scope module: :public do
       # root 'items#top'
      resources :items, only: [:show, :index]
