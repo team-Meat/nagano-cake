b00d32cba10d01dc63a6e148c4761403fb606d35
Tisima <zerosan487@gmail.com>
2022-07-24 07:08:19 +0000

    AddCustomerCaram

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_07_23_110212) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -64,6 +63,14 @@ ActiveRecord::Schema.define(version: 2022_07_23_110212) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postcode"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_deleted"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -72,32 +79,6 @@ ActiveRecord::Schema.define(version: 2022_07_23_110212) do
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
-
-  create_table "items", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "genre_id", null: false
-    t.string "name", null: false
-    t.text "introduction", null: false
-    t.integer "price", null: false
-    t.boolean "is_active", default: true, null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "item_id"
-    t.integer "postage", null: false
-    t.integer "delivery_postcode", null: false
-    t.string "delivery_adress", null: false
-    t.string "delivery_name", null: false
-    t.integer "billing_amount", null: false
-    t.integer "order_status", default: 0, null: false
-    t.integer "payment_method", null: false
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
@@ -159,5 +140,4 @@ ActiveRecord::Schema.define(version: 2022_07_23_110212) do
   add_foreign_key "item_orders", "orders"
   add_foreign_key "items", "genres"
   add_foreign_key "shippings", "publics"
-
 end
