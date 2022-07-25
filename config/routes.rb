36c9c0573a735afaa6e46abc62e0617d424368cb
EC2 Default User <krat1021007@gmail.com>
2022-07-25 12:33:33 +0000

    会員ステータス

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
   end
 
+
   namespace :admin do
     root :to =>"homes#top"
 
