f1d746e9fd1378156326d929a8198385b19fe7af
Tisima <zerosan487@gmail.com>
2022-07-26 04:34:33 +0000

    726

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -10,4 +10,9 @@ class Public::ItemsController < ApplicationController
     @cart_item = CartItem.new
   end
 
+  private
+  def item_params
+  	  params.require(:item).permit(:genre_id, :name, :price, :explanation)
+  end
 end
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,14 @@
 
 import "channels"
 
+import Rails from "@rails/ujs"
+
+import Turbolinks from "turbolinks"
+
+import * as ActiveStorage from "@rails/activestorage"
+
+import "channels"
+
 import "jquery";
 import "popper.js";
 import "bootstrap";
@@ -14,5 +22,5 @@ import "../stylesheets/application"
  Rails.start()
  Turbolinks.start()
  ActiveStorage.start()
-=======
+
 
