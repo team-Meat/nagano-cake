615e485a29718b31b1af24a4bbef06e1cecab08d
EC2 Default User <krat1021007@gmail.com>
2022-07-26 04:19:42 +0000

    header編集show編集

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,13 +3,8 @@ class Public::SearchesController < ApplicationController
   def search
    @range = params[:range]
 
-   if @range == "User"
-      @users =User.looks(params[:search],params[:word])
-
-   else
-     @books = Book.looks(params[:search],params[:word])
-
-
+   if @range == "items"
+      @items =Item.looks(params[:search],params[:word])
 
    end
   end
