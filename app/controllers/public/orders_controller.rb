1bed85cc20939e0f99280f15e53a49c806cb3175
Tisima <zerosan487@gmail.com>
2022-07-27 02:47:54 +0000

    AddOrderview

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,13 +1,17 @@
 class Public::OrdersController < ApplicationController
 
   def new
-@order = Order.new
+		@order = Order.new
 		@customer = current_customer
-		@addresses = ShippingAddress.where(customer_id: current_customer.id)
+		@addresses = Shipping.where(customer_id: current_customer.id)
   end
 
   def confirm
-		@cart_items = current_customer.cart_items
+     @cart_items = current_customer.cart_items.all
+     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+     @customer = current_customer
+     @order = Order.new(order_params)
+     @addresses = Shipping.where(customer_id: current_customer.id)
   end
 
   def index
@@ -19,4 +23,8 @@ class Public::OrdersController < ApplicationController
     @order_details = @order.order_details
   end
 
+  private
+  def order_params
+  	  params.require(:order).permit(:customer_id, :shipping_id, :receive_name, :postal_code, :street_address, :postage, :payment, :total_price, :order_status)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,7 @@ class Customer < ApplicationRecord
 
          has_many :cart_items, dependent: :destroy
          has_many :orders, dependent: :destroy
-         has_many :shipping_addresses, dependent: :destroy
+         has_many :shippings, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
